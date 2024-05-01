43a27dda402af0c75107ec6e75056c19d760fac5
pranjal singh sengar <67635283+pranjalsinghsengar@users.noreply.github.com>
2024-05-02 00:54:44 +0530

    --u

--- src/App.js
+++ src/App.js
@@ -28,35 +28,48 @@ function App() {
   ];
 
   return (
-    <div className=' w-screen h-screen '>
+    <div className=' w-screen h-screen bg-zinc '>
+      <div
+        className='w-full h-full absolute'
+        style={{
+          width: "100vw",
+          height: "100vh",
+          backgroundImage:
+            'url("https://images.unsplash.com/photo-1493514789931-586cb221d7a7?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      ></div>
       <div className='relative w-full h-full overflow-hidden flex items-center flex-col justify-between'>
-        <img
-          className='absolute opacity-90'
+        {/* <img
+          className='opacity-90 fixed'
           src='https://images.unsplash.com/photo-1493514789931-586cb221d7a7?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
           alt=''
-        />
-        <div className='flex justify-between items-center w-11/12 relative'>
-          <h1 className='text-2xl uppercase tracking-[1rem] border-b border-zinc-400 pb-2 my-14 text-zinc-400'>
+        /> */}
+
+        <div className='flex justify-center md:justify-between items-center w-11/12 relative'>
+          <h1 className='text-lg md:text-2xl uppercase tracking-[1rem] border-b border-zinc-400 pb-2 my-14 text-zinc-400'>
             sengar
           </h1>
-          <button className='border border-white text-white tracking-wide px-10 py-2 rounded-lg'>
+          <button className='hidden lg:block border border-white text-white tracking-wide px-10 py-2 rounded-lg'>
             login
           </button>
         </div>
 
-        <div className='px-10 py-5 rounded text-white bg-zinc-800 relative font-semibold capitalize tracking-wide'>
+        <div className='px-5 py-5 mx-10 md:m-0 md:px-10 md:py-5 rounded text-white bg-zinc-800 relative font-semibold capitalize tracking-wide'>
           join our group and grab the opportunity, we share freelancing project
         </div>
 
         <div className='w-full flex  justify-center items-center h-1/4 bg-zinc-800 relative'>
-          <h1 className='absolute text-white bg-zinc-800 border px-7 py-2 text-sm rounded-full -top-5 capitalize tracking-widest'>
+          <h1 className='absolute text-white bg-zinc-800 border px-7 py-2 text-xs md:text-sm rounded-full -top-5 capitalize tracking-widest'>
             this site is under construction
           </h1>
           <div className='w-full  flex flex-col items-center h-4/5 mt-7'>
             <h1 className='text-white text-xl capitalize tracking-widest mb-14'>
               join over community
             </h1>
-            <div className='flex justify-evenly w-6/12'>
+            <div className='flex justify-evenly md:w-6/12'>
               {buttons.map((item, index) => {
                 return (
                   <a
@@ -64,11 +77,11 @@ function App() {
                     key={index}
                   >
                     <button
-                      className='capitalize text-3xl text-white px-5 py-2 flex gap-1 flex-col items-center '
+                      className='capitalize text-xl md:text-3xl text-white px-5 py-2 flex gap-1 flex-col items-center '
                       onClick={item?.link === null ? notify : item?.link}
                     >
                       {item.icon}
-                      <span className='text-lg hover:border-b'>
+                      <span className='text-sm md:text-lg hover:border-b'>
                         {item.title}
                       </span>
                     </button>
